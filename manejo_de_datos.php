8cef7a3de8c6355fbfa1eb1348f63d204f2c11e2
nameDNNNY/name <132737160+DNNNY-FOX@users.noreply.github.com>
2023-05-27 20:06:53 -0600

    cambios al archivo .php

--- manejo_de_datos.php
+++ manejo_de_datos.php
@@ -4,4 +4,12 @@
     Manejo de datos por: Esteban Flores Martínez.
 */
 
+//Matriz universal que almacenará los datos de los usuarios
+$usuarios = array();
+
+//Iniciar un usuario de prueba en la matriz
+$usuarios[] = array(
+    "id" => "Esteban",
+)
+
 ?>
